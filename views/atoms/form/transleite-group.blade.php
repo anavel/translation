78a19efc05cde99361a2220a36e2f18cb1865ecb
Carlos Morales <carlos@anavallasuiza.com>
2015-12-03 19:00:31 +0100

    Updated views

---
+++ views/atoms/form/transleite-group.blade.php
@@ -0,0 +1,21 @@
+<fieldset>
+    <legend>{{ $groupTitle }}</legend>
+    @foreach ($group as $childLineKey => $childLine)
+        @if(is_array($group))
+            @include('atoms.form.transleite-group', [
+                'groupTitle' => $groupTitle . ' | ' .$lineKey,
+                'group' => $childLine,
+                'formElementID' => "{$formElementID}[{$langKey}]"
+            ])
+        @else
+            <div class="form-group transleite-group">
+                <label for="{!! $formElementID !!} }}[{{ $childLineKey }}]"
+                       class="control-label col-lg-4">{{ $childLineKey }}</label>
+
+        <textarea id="{!! $formElementID !!} }}[{{ $childLineKey }}]"
+                  name="{!! $formElementID !!} }}[{{ $childLineKey }}"
+                  class="form-control col-lg-8">{!! $childLine !!}</textarea>
+            </div>
+        @endif
+    @endforeach
+</fieldset>
--- views/pages/edit.blade.php
+++ views/pages/edit.blade.php
@@ -12,77 +12,71 @@
 @stop
 
 @section('breadcrumb')
-    <ol class="breadcrumb">
-        <li><a href="{{ route('adoadomin-transleite.edit') }}"><i class="fa fa-language"></i> {{ config('adoadomin-transleite.name') }}</a></li>
-        <li class="active">{{ trans('adoadomin-transleite::messages.edit_title') }}</li>
-    </ol>
+    {{--<ol class="breadcrumb">--}}
+    {{--<li><a href="{{ route('adoadomin-transleite.edit') }}"><i class="fa fa-language"></i> {{ config('adoadomin-transleite.name') }}</a></li>--}}
+    {{--<li class="active">{{ trans('adoadomin-transleite::messages.edit_title') }}</li>--}}
+    {{--</ol>--}}
 @stop
 
 @section('content')
     @if(! empty($editLangs))
-    <div class="nav-tabs-custom">
-        <form method="post" action="{{ route('adoadomin-transleite.update', $current) }}" id="transleite-form">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            <input type="hidden" name="_method" value="PUT">
+        <div class="nav-tabs-custom">
+            <form method="post" action="" id="transleite-form">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <input type="hidden" name="_method" value="PUT">
 
-            <ul class="nav nav-tabs">
-                @foreach ($locales as $locale)
-                    <li {!! ($locale === $current) ? 'class="active"' : '' !!}><a href="{{ route(Route::currentRouteName(), $locale) }}">{{ $locale }}</a></li>
-                @endforeach
-
-                <li class="pull-right">
-                    <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> {{ trans('adoadomin-transleite::messages.save_button') }}</button>
-                </li>
-            </ul>
-
-            <div class="tab-content">
-                <div class="tab-pane active">
-                    <div class="well">
-                        <div class="row">
-                            <div class="col-lg-8">
-                                <input type="search" placeholder="Search (min. 3 chars)..." class="form-control" />
-                            </div>
-
-                            <div class="col-lg-4">
-                                <div class="btn-group pull-right" data-toggle="buttons">
-                                    <label class="btn">
-                                        <input type="checkbox" data-transleite="empty" autocomplete="off" /> Only empty
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <?php $i = 0; ?>
-
-                    @foreach ($entries as $entry)
-                        <div class="form-group transleite-group">
-                            <label for="entry-{{ ++$i }}">{{{ $entry->getOriginal() }}}</label>
-
-                            @if ($entry->lines)
-                                <a href="#" class="fa fa-info-circle show-references"></a>
-
-                                <ul class="list-unstyled references text-muted" style="display: none;">
-                                    <li>{!! implode('</li><li>', $entry->lines) !!}</li>
-                                </ul>
-                            @endif
-
-                            <textarea id="entry-{{ $i }}" name="translations[{{ $entry->getOriginal() }}]" class="form-control">{{ $entry->getTranslation() }}</textarea>
-                        </div>
+                <ul class="nav nav-tabs">
+                    @foreach (array_keys($editLangs) as $locale)
+                        <li><a href="{{ '#' . $locale }}" role="tab" aria-controls="{{ $locale }}">{{ $locale }}</a>
+                        </li>
                     @endforeach
 
-                    <div class="box-footer clearfix">
-                        <button type="submit" class="btn btn-primary pull-right"><i class="fa fa-save"></i> {{ trans('adoadomin-transleite::messages.save_button') }}</button>
-                    </div>
+                    <li class="pull-right">
+                        <button type="submit" class="btn btn-primary"><i
+                                    class="fa fa-save"></i> {{ trans('adoadomin-transleite::messages.save_button') }}
+                        </button>
+                    </li>
+                </ul>
+
+                <div class="tab-content">
+                    @foreach($editLangs as $langKey => $langLines)
+                        <div role="tabpanel" class="tab-pane" id="{{ $langKey }}">
+                            <div class="form-horizontal">
+                                @foreach ($langLines as $lineKey => $line)
+                                    @if(is_array($line))
+                                        @include('atoms.form.transleite-group', [
+                                            'groupTitle' => $lineKey,
+                                            'group' => $line,
+                                            'formElementID' => "translations[$langKey]"
+                                        ])
+                                    @else
+                                        <div class="form-group transleite-group">
+                                            <label for="translations[{{ $langKey }}][{{ $lineKey }}]"
+                                                   class="control-label col-lg-4">{{ $lineKey }}</label>
+
+                                        <textarea id="translations[{{ $langKey }}][{{ $lineKey }}]"
+                                                  name="translations[{{ $langKey }}][{{ $lineKey }}]"
+                                                  class="form-control col-lg-8">{!! $line !!}</textarea>
+                                        </div>
+                                    @endif
+                                @endforeach
+                            </div>
+
+                            <div class="box-footer clearfix">
+                                <button type="submit" class="btn btn-primary pull-right"><i
+                                            class="fa fa-save"></i> {{ trans('adoadomin-transleite::messages.save_button') }}
+                                </button>
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
-            </div>
-        </form>
-    </div>
+            </form>
+        </div>
     @endif
 @stop
 
-@section('footer-scripts')
-    @parent
+{{--@section('footer-scripts')--}}
+{{--@parent--}}
 
-    <script src="{{ asset('vendor/adoadomin-transleite/js/app.js') }}" type="text/javascript"></script>
-@stop
+{{--    <script src="{{ asset('vendor/adoadomin-transleite/js/app.js') }}" type="text/javascript"></script>--}}
+{{--@stop--}}
